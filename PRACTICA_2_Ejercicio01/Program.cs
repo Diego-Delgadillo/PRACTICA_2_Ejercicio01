6fe27c09f409835d3c06b784b9b24bba974c1394
Diego-Delgadillo <drdg2005@gmail.com>
2024-04-13 20:35:24 -0600

    Se añadio la correcion para solo aceptar numeros en el menu de opcion

--- PRACTICA_2_Ejercicio01/Program.cs
+++ PRACTICA_2_Ejercicio01/Program.cs
@@ -15,16 +15,29 @@ public class Program
     public static void Main()
     {
         //declaramos las variables de control
-        int opcion = 0;
-        bool opcionMenu = true;
+        int opcion; //variable para la opcion del switch
+        string cadena; //varaible que usa el metodo tryparse y compara si es string
+        bool num; 
+        bool opcionMenu = true; //varaible para el menu del programa
 
        
         while (opcionMenu == true)
         {
             //inicia con un mensaje de lo que hace el programa
-            Console.WriteLine("Este programa permite obtner el área y perimetro\n");
-            Console.WriteLine("Seleccione la figura a calcular:\n 1.Cuadrado\n 2.Rectángulo\n 3.Triángulo\n 4.Círculo\n 5.Rombo\n 6.Trapecio\n 7.Polígono regular\n");
-            opcion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Este programa permite obtener el área y perímetro\n");
+
+
+            //ciclo para almacenar solamente números 
+            do
+            {
+                Console.WriteLine("Seleccione la figura a calcular:\n 1.Cuadrado\n 2.Rectángulo\n 3.Triángulo\n 4.Círculo\n 5.Rombo\n 6.Trapecio\n 7.Polígono regular\n");
+                cadena = Console.ReadLine();
+                num = int.TryParse(cadena, out opcion);
+            }
+            while (!num);
+
+
+            opcion = Convert.ToInt32(cadena);
 
             switch (opcion)
             {
@@ -34,17 +47,35 @@ public class Program
                     break;
 
 
-                case 2: Console.WriteLine("\nRECTÁNGULO"); break;
+                case 2: Console.WriteLine("\nRECTÁNGULO");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
-                case 3: Console.WriteLine("\nTRIÁNGULO"); break;
+                case 3: Console.WriteLine("\nTRIÁNGULO");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
-                case 4: Console.WriteLine("\nCÍRCULO"); break;
+                case 4: Console.WriteLine("\nCÍRCULO");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
-                case 5: Console.WriteLine("\nROMBO"); break;
+                case 5: Console.WriteLine("\nROMBO");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
-                case 6: Console.WriteLine("\nTRAPECIO"); break;
+                case 6: Console.WriteLine("\nTRAPECIO");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
-                case 7: Console.WriteLine("\nPOLÍGONO REGULAR"); break;
+                case 7: Console.WriteLine("\nPOLÍGONO REGULAR");
+                    Console.WriteLine("Para continuar ingrese (1), para salir ingrese (0): ");
+                    opcionMenu = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine())); //asigna el valor ya sea true or false
+                    break;
 
                 default: Console.WriteLine("Ingrese  una opción válida!"); Console.Beep(); break;
             }
